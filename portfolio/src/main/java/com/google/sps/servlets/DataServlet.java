659fdf5490a14b7a3538bca9d5f98efb2d61eb1a
Ariel Salazar <arielsalazarraw@gmail.com>
2020-02-29 18:35:51 -0500

    Add JavaScript code to fetch data

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,10 +23,10 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String msg = "Hello Ariel Salazar!";
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Ariel Salazar!</h1>");
+    response.getWriter().println(msg);
   }
 }
