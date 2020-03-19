48d7d438ff382c365a5905ec253e9a265b1085f3
Ariel Salazar <arielsalazarraw@gmail.com>
2020-03-19 10:07:19 -0400

    Simplify FindMeetingQuery Algorithm

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,68 +1,66 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import java.util.Collection;
+package com.google.sps;
+
 import java.util.Collections;
+import java.util.Collection;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 
 // TIME COMPLEXITY: O(E * R), where E is the number of events and R is the number of members in the request
 
 public final class FindMeetingQuery {
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-        if (request.getDuration() > 1440)
+        if (request.getDuration() > TimeRange.WHOLE_DAY.end())
             return new ArrayList<>();
         else if (events == null || events.isEmpty())
-            return new ArrayList<>(Collections.singletonList(TimeRange.fromStartEnd(0, 1440, false)));
+            return new ArrayList<>(Collections.singletonList(TimeRange.WHOLE_DAY));
         else{
-            List<List<Integer>> meetingTimeRanges = new ArrayList<>();
-
-            for (Event singleEvent: events){
-                if (requestedMemberInExistingMeeting(request.getAttendees(), singleEvent.getAttendees()))
-                    meetingTimeRanges.add(Arrays.asList(singleEvent.getWhen().start(), singleEvent.getWhen().end()));
-            }
-
-            int i = 0;
-            while (i < meetingTimeRanges.size()-1){
-                if (overlaps(meetingTimeRanges.get(i), meetingTimeRanges.get(i+1)))
-                    meetingTimeRanges.add(i, joinOverlappingTimeRanges(meetingTimeRanges.remove(i), meetingTimeRanges.remove(i)));
-                else
-                    i++;
-            }
+            ArrayList<TimeRange> meetingTimeRanges = new ArrayList<>();
+            for (Event event: events){
+                if (!Collections.disjoint(request.getAttendees(), event.getAttendees())){
+                    TimeRange eventTimeRange = event.getWhen();
+                    if (meetingTimeRanges.size() == 0)
+                        meetingTimeRanges.add(eventTimeRange);
+                    else{
+                        int lastIndex = meetingTimeRanges.size()-1;
+                        TimeRange lastMeetingTime = meetingTimeRanges.get(lastIndex);
+                        if (eventTimeRange.overlaps(lastMeetingTime))
+                            meetingTimeRanges.set(lastIndex, joinTimes(eventTimeRange, lastMeetingTime));
+                        else
+                            meetingTimeRanges.add(eventTimeRange);
+                    }
+                }
+            } // For loop adds all the meeting time ranges of the day into an ArrayList
 
             ArrayList<TimeRange> freeTimeSlots = new ArrayList<>();
 
             int startTime = 0;
             int endTime;
-            for (List<Integer> singleMeetingTime: meetingTimeRanges){
-                endTime = singleMeetingTime.get(0);
+            for (TimeRange singleMeetingTime: meetingTimeRanges){ // O(E)
+                endTime = singleMeetingTime.start();
                 if (endTime - startTime >= request.getDuration())
                     freeTimeSlots.add(TimeRange.fromStartEnd(startTime, endTime, false));
-                startTime = singleMeetingTime.get(1);
-            }
-            if (startTime < 1440)
-                freeTimeSlots.add(TimeRange.fromStartEnd(startTime, 1440, false));
+                startTime = singleMeetingTime.end();
+            } // For loop adds all the free time slots (all the gaps between the meetings) to freeTimeSlots
+            if (startTime < TimeRange.WHOLE_DAY.end())
+                freeTimeSlots.add(TimeRange.fromStartEnd(startTime, TimeRange.WHOLE_DAY.end(), false));
 
             return freeTimeSlots;
         }
     }
-    public static boolean requestedMemberInExistingMeeting(Collection<String> requestedMembers, Set<String> eventMembers){
-        for (String singleRequestedMember: requestedMembers){
-            if (eventMembers.contains(singleRequestedMember))
-                return true;
-        }
-        return false;
-    }
-    public static boolean overlaps(List<Integer> meetingA, List<Integer> meetingB){
-        if (meetingA.get(0) > meetingB.get(0) && meetingA.get(0) < meetingB.get(1))
-            return true;
-        else if (meetingA.get(1) > meetingB.get(0) && meetingA.get(1) < meetingB.get(1))
-            return true;
-        else if (meetingB.get(0) > meetingA.get(0) && meetingB.get(0) < meetingA.get(1))
-            return true;
-        else return meetingB.get(1) > meetingA.get(0) && meetingB.get(1) < meetingA.get(1);
-    }
-    public static List<Integer> joinOverlappingTimeRanges(List<Integer> meetingA, List<Integer> meetingB) {
-        return Arrays.asList(Math.min(meetingA.get(0), meetingB.get(0)), Math.max(meetingA.get(1), meetingB.get(1)));
+    public static TimeRange joinTimes(TimeRange meetingA, TimeRange meetingB) {
+        return TimeRange.fromStartEnd(Math.min(meetingA.start(), meetingB.start()), Math.max(meetingA.end(), meetingB.end()), false);
     }
 }
